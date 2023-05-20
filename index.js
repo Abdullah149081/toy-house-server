da0a86a2fd15883e346baee7b8177ed125253040
Abdullah149081 <masudmd425@gmail.com>
2023-05-20 10:05:48 +0600

    middleware set is done

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
 app.get("/", (req, res) => {
   res.send("toy house server is running");
 });
